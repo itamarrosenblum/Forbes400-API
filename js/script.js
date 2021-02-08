c7155f09b94ced67f21b2d03ee293e40001016d5
Itamar Rosenblum <itamarfel10@gmail.com>
2021-02-08 10:19:49 +0200

    Fetch fix
    'try' and 'catch' has been aded

--- js/script.js
+++ js/script.js
@@ -8,72 +8,77 @@ Last Update: None
 
 // Async await function
 (async function fn() {
-  // Fetch to forbes400 API
-  const res = await fetch("https://forbes400.herokuapp.com/api/forbes400?limit=10");
-  const data = await res.json();
-console.log(data);
-  // Creating the table
-  for (let i = 0; i < data.length; i++) {
-    // Get element
-    const tbody = document.querySelector("tbody");
+    try {
+    // Fetch to forbes400 API
+    const res = await fetch("https://forbes400.herokuapp.com/api/forbes400?limit=10");
+    const data = await res.json();
 
-    // Create tr element
-    const tr = document.createElement("tr");
+    // Creating the table
+    for (let i = 0; i < data.length; i++) {
+      // Get element
+      const tbody = document.querySelector("tbody");
 
-    // Create img table data
-    const tdImage = document.createElement("td");
-    const img = document.createElement("img");
-    tdImage.appendChild(img);
-    // Fixing https:// bug of the API
-    if(data[i].squareImage.indexOf("https") === -1) {
-      img.src = `https:${data[i].squareImage}`;
-    } else {
-      img.src = data[i].squareImage;
+      // Create tr element
+      const tr = document.createElement("tr");
+
+      // Create img table data
+      const tdImage = document.createElement("td");
+      const img = document.createElement("img");
+      tdImage.appendChild(img);
+      
+      // Fixing https:// bug of the API
+      if(data[i].squareImage.indexOf("https") === -1) {
+        img.src = `https:${data[i].squareImage}`;
+      } else {
+        img.src = data[i].squareImage;
+      }
+      tr.appendChild(tdImage);
+
+      // Create rank table data
+      const tdRank = document.createElement("td");
+      const pRank = document.createElement("p");
+      pRank.innerText = `${data[i].rank}`;
+      tdRank.appendChild(pRank);
+      tr.appendChild(tdRank);
+
+      // Create name table data
+      const tdName = document.createElement("td");
+      tdName.innerText = data[i].personName;
+      tr.appendChild(tdName);
+      
+      // Create networth table data
+      const tdNetWorth = document.createElement("td");
+      // Round a number
+      const netNum = Math.floor(data[i].finalWorth);
+      // Turning number to string
+      const netStr = netNum.toString();
+      // Abbreviate large numbers
+      if (netStr.length > 5) {
+          tdNetWorth.innerText = `$${Number(netStr.slice(0, 3))} B`;
+      } else if (netStr.length <= 5) {
+          tdNetWorth.innerText = `$${Number(netStr.slice(0, 2))} B`;
+      }
+      tr.appendChild(tdNetWorth);
+
+      // Create gender table data
+      const tdGender = document.createElement("td");
+      tdGender.innerText = data[i].gender;
+      tr.appendChild(tdGender);
+
+      // Create residence table data
+      const tdResidence = document.createElement("td");
+      tdResidence.innerText = data[i].countryOfCitizenship;
+      tr.appendChild(tdResidence);
+
+      // Create source table data
+      const tdSource = document.createElement("td");
+      tdSource.innerText = data[i].source;
+      tr.appendChild(tdSource);
+
+      // Append tr to tbody
+      tbody.appendChild(tr);
     }
-    tr.appendChild(tdImage);
-
-    // Create rank table data
-    const tdRank = document.createElement("td");
-    const pRank = document.createElement("p");
-    pRank.innerText = `${data[i].rank}`;
-    tdRank.appendChild(pRank);
-    tr.appendChild(tdRank);
-
-    // Create name table data
-    const tdName = document.createElement("td");
-    tdName.innerText = data[i].personName;
-    tr.appendChild(tdName);
-    
-    // Create networth table data
-    const tdNetWorth = document.createElement("td");
-    // Round a number
-    const netNum = Math.floor(data[i].finalWorth);
-    // Turning number to string
-    const netStr = netNum.toString();
-    // Abbreviate large numbers
-    if (netStr.length > 5) {
-        tdNetWorth.innerText = `$${Number(netStr.slice(0, 3))} B`;
-    } else if (netStr.length <= 5) {
-        tdNetWorth.innerText = `$${Number(netStr.slice(0, 2))} B`;
-    }
-    tr.appendChild(tdNetWorth);
-
-    // Create gender table data
-    const tdGender = document.createElement("td");
-    tdGender.innerText = data[i].gender;
-    tr.appendChild(tdGender);
-
-    // Create residence table data
-    const tdResidence = document.createElement("td");
-    tdResidence.innerText = data[i].countryOfCitizenship;
-    tr.appendChild(tdResidence);
-
-    // Create source table data
-    const tdSource = document.createElement("td");
-    tdSource.innerText = data[i].source;
-    tr.appendChild(tdSource);
-
-    // Append tr to tbody
-    tbody.appendChild(tr);
+  } catch (err) {
+    console.error(err);
   }
 })();
